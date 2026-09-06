e585fc040f30c65018f1adc5669f7428591819aa
bunny98789 <Bunnyyyyy80@gmail.com>
2026-09-06 17:38:59 -0400

    Change MOVE_INTERVAL to 25 for slower movement updates

--- game.js
+++ game.js
@@ -1272,7 +1272,7 @@ document.addEventListener("keyup", (e) => {
 });
 
 let lastMoveTime = 0;
-const MOVE_INTERVAL = 16; // 60 movement updates per second
+const MOVE_INTERVAL = 25; // 40 movement updates per second
 
 function movementLoop(timestamp) {
 
