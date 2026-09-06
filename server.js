e986bda2f2ff841f955f300d2a1f35ba9d5a44a8
bunny98789 <Bunnyyyyy80@gmail.com>
2026-09-06 17:39:35 -0400

    Change player update interval to 25 milliseconds
    Increased the interval for player updates from 16 to 25 milliseconds.

--- server.js
+++ server.js
@@ -2627,7 +2627,7 @@ setInterval(() => {
             io.to(roomCode).emit("updatePlayers", room.players);
         }
     }
-}, 16);
+}, 25);
 
 setInterval(() => {
 
@@ -2647,7 +2647,7 @@ setInterval(() => {
 
     }
 
-}, 16);
+}, 25);
 
 // ========================================
 // BULLET LOOP
